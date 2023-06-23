7f4c563f5416bb65c36470e4f4a2215e25322866
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-06-23 17:51:07 +0300

    Add function "playSingleRound"
    Added a function that will compare the computer and player choices and
    declare the winner based on the result. Reworked the way "getComputerChoice"
    and "getPlayerChoice" functions return values so that a separate variable
    is not used in the process.

--- js/javascript.js
+++ js/javascript.js
@@ -3,29 +3,55 @@ function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3) + 1
     console.log(randomNum);
     if (randomNum === 1) {
-        return computerSelection = "rock" 
+        return "rock" 
     } else if (randomNum === 2) {
-        return computerSelection = "paper"
+        return "paper"
     } else {
-        return computerSelection = "scissors"
+        return "scissors"
     }
 }
 
+let computerSelection = getComputerChoice()
+console.log(`Computer chose: ${computerSelection}`);
 
-
-console.log(getComputerChoice())
 
 // Allow player to choose "Rock", "Paper" or "Scissors" and make the choice case insensitive
-    function getPlayerChoice() {
-        let playerSelection = prompt(`Choose "rock", "paper" or "scissors"`)
-        return playerSelection.toLowerCase()
-    }
 
-console.log(getPlayerChoice());
+function getPlayerChoice() {
+    let selectItem = prompt(`Choose "rock", "paper" or "scissors"`)
+    return selectItem.toLowerCase()
+}
+
+let playerSelection = getPlayerChoice()
+console.log(`Player chose: ${playerSelection}`);
+
 // Compare players choice against the computers
 function playSingleRound(playerSelection, computerSelection) {
-    
+    if (playerSelection == computerSelection) {
+        console.log(playerSelection === computerSelection);
+        return winner = "It's a tie."
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return winner = "You Win! Rock beats Scissors."
+        } else {
+            return winner = "You Lose! Paper beats Rock"
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return winner = "You Win! Paper beats Rock"
+        } else {
+            return winner = "You Lose! Scissors beat Paper"
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return winner = "You Win! Scissors beat Paper"
+        } else {
+            return winner = "You Lose! Rock beats Scissors"
+        }
+    }
 }
+
+console.log(playSingleRound(playerSelection, computerSelection));
 // Determine the winner
 // Print out the result
 // Reset the game
