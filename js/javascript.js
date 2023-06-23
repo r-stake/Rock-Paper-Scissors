9f07ae243b832b6d714036f94d83fed53454cd32
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-06-23 16:17:59 +0300

    Create getPlayerSelection function
    Function prompts the user for a string and converts it to lowercase characters

--- js/javascript.js
+++ js/javascript.js
@@ -3,18 +3,29 @@ function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3) + 1
     console.log(randomNum);
     if (randomNum === 1) {
-        return "rock" 
+        return computerSelection = "rock" 
     } else if (randomNum === 2) {
-        return "paper"
+        return computerSelection = "paper"
     } else {
-        return "scissors"
+        return computerSelection = "scissors"
     }
 }
 
+
+
 console.log(getComputerChoice())
 
-// Allow player to choose "Rock", "Paper" or "Scissors"
+// Allow player to choose "Rock", "Paper" or "Scissors" and make the choice case insensitive
+    function getPlayerChoice() {
+        let playerSelection = prompt(`Choose "rock", "paper" or "scissors"`)
+        return playerSelection.toLowerCase()
+    }
+
+console.log(getPlayerChoice());
 // Compare players choice against the computers
+function playSingleRound(playerSelection, computerSelection) {
+    
+}
 // Determine the winner
 // Print out the result
 // Reset the game
